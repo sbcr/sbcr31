18c5580c3883ba1f34be8764c63b116e7a42f6fa
Sam Bond <sambond@new-host-3.home>
2011-07-23 16:24:10 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sbcr31::Application.config.secret_token = 'db81f4145332049e6125fa05be16415e9ecd1a56a66edc1016ea92a0330a18f54d6a851efadc46b1f121ab8d1dcb8338683d16d99cc983b3bae44fb3ca09cb69'
